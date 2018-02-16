ca1734400ea5f686a93f94bcbfcc56e210d1e797
Dariell Vasquez <dariell@Dariells-Air.fios-router.home>
2018-02-15 22:37:57 -0500

    Heroku bug

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -5,8 +5,8 @@ const ID_TOKEN_KEY = 'id_token';
 const ACCESS_TOKEN_KEY = 'access_token';
 const CLIENT_ID = 'F3bv6-7PmyBo-GCCxiyewVN_Yf-CPOlg';
 const CLIENT_DOMAIN = 'rembr-app.auth0.com';
-const REDIRECT = 'http://localhost:3000/callback';
-// const REDIRECT = 'https://rembr-app.herokuapp.com/callbacks';
+// const REDIRECT = 'http://localhost:3000/callback';
+const REDIRECT = 'https://rembr-app.herokuapp.com/callback';
 const SCOPE = 'openid profile email';
 const AUDIENCE = 'https://rembr-app.auth0.com/api/v2/';
 
