7ef5e220054b50c58c836cdfeb2c0314a8e0b41b
Dariell Vasquez <dariellv@yahoo.com>
2018-10-24 16:31:07 -0400

    refactored priority article rendering, working

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -37,46 +37,40 @@ class Articles extends React.Component {
   componentDidMount() {
     // callback function to retrieve the user's email from the AuthService file
     getUserInfo(email=> {
-      this.setState({
-        email: email
-      })
-      this.loadBooks(email);
+      this.setState({email});
+      this.props.getArticles(email)
     }) 
   }
 
-  // Loads all books  and sets them to this.state.books
-  loadBooks = (email) => {
-    this.props.getArticles(email)
-      .then(res => {
-        console.log(res)
-        this.setState({ articles: res.data})
-      }
-      )
-      .catch(err => console.log(err));
-  };
+  // // Loads all articles
+  // loadBooks = (email) => {
+    
+  //     .then()
+  //     .catch(err => console.log(err));
+  // };
 
-  // Deletes a book from the database with a given id, then reloads books from the db
-  deleteArticle = id => {
-    API.deleteArticle(id)
-      .then(res => this.loadBooks(this.state.email))
-      .catch(err => console.log(err));
-  };
+  // // Deletes a book from the database with a given id, then reloads books from the db
+  // deleteArticle = id => {
+  //   API.deleteArticle(id)
+  //     .then(res => this.loadBooks(this.state.email))
+  //     .catch(err => console.log(err));
+  // };
 
-  saveForLater = (id, decision) => {
-    API.saveForLater(id, decision)
-      .then(res => {
-        this.loadBooks(this.state.email);
-      })
-      .catch(err => console.log(err));
-  };
+  // saveForLater = (id, decision) => {
+  //   API.saveForLater(id, decision)
+  //     .then(res => {
+  //       this.loadBooks(this.state.email);
+  //     })
+  //     .catch(err => console.log(err));
+  // };
 
-  favoriteArticle = (id, decision) => {
-    API.favoriteArticle(id, decision)
-    .then(res => {
-      this.loadBooks(this.state.email);
-    })
-      .catch(err => console.log(err));
-  };
+  // favoriteArticle = (id, decision) => {
+  //   API.favoriteArticle(id, decision)
+  //   .then(res => {
+  //     this.loadBooks(this.state.email);
+  //   })
+  //     .catch(err => console.log(err));
+  // };
 
   handleTagChange = (e, articleId) => {
     const {value} = e.target
@@ -103,8 +97,50 @@ class Articles extends React.Component {
     })
       .catch(err => console.log(err));
   }
+  renderLeftArticles = (priority) => {
+    return (
+      <List>
+        <h3 style={{textAlign:"center"}}>Priority</h3>
+        {priority.map(article => {
+        return (         
+            <ListItem key={article._id}>
+              <a className="article-url" href={article.url}>
+                <strong><h4> {article.title} </h4></strong>
+              </a>
+              <p> Viewed: {article.date.split("T")[0]} </p>  
+              <p>Tags:</p>
+              <ul>{article.tags.length === 0 
+                ? 
+                <form style={{padding:"12px"}} onSubmit={(e) => {
+                  this.handleSubmit(e, article._id);
+                }}>
+                    <input type="text" onChange={this.handleTagChange} className="form-control" placeholder="Enter a tag"/>
+                </form>
+                :
+                article.tags.map((tag, i)=> <li key={i}>{tag}</li>
+                )}
+              </ul>
+              <IconsContainer noteId="note" note={article.note}/>
+              <IconsContainer value={article.favorited} favoriteId="favorite"> 
+                <FavoriteBtn type="favorite" value={article.favorited} onClick={() => this.favoriteArticle(article._id, !article.favorited)}/> 
+              </IconsContainer>
+              <IconsContainer backlogId="backlog">
+                <BacklogBtn type="toBacklog" onClick={() => this.saveForLater(article._id, true)} />
+              </IconsContainer>
+              <DeleteBtn onClick={() => this.deleteArticle(article._id)} />
+            </ListItem>
+          );
+      })}
+    </List>
+
+    )
+    
+  }
 
   render() {
+    const {articles} = this.props; 
+    const priority = _.filter(articles, article=> !article.saveForLater);
+    const backlog = _.filter(articles, article=> article.saveForLater); 
     // let priority = this.props.articles.filter(article=> !article.saveForLater);
     // let backlog = this.props.articles.filter(article=> article.saveForLater);           
     return (
@@ -118,49 +154,17 @@ class Articles extends React.Component {
           <Banner/>
             <Row>
             {!isLoggedIn() && 
-            <Col size="md-12"> <h3 className="text-center">Please log in to view your articles.</h3> </Col>}
-            {!this.props.articles.length && isLoggedIn() ? 
-            <Col size="md-12"><h3 className="text-center">Please save articles using the extension to view them here.</h3></Col> : ""}
-            {isLoggedIn() && this.props.articles.length ?
+            <Col size="md-12"> <h3 className="text-center">Please log in to view your articles.</h3> 
+            </Col>}
+            {_.size(articles) === 0 && isLoggedIn() ? 
+            <Col size="md-12"><h3 className="text-center">Please save articles using the extension to view them here.</h3>
+            </Col> : ""}
+            {isLoggedIn() && _.size(articles) > 0 ?
              <div>
               <Col styleProp="left-articles" size="md-4">
-              {/* {priority.length ? (
-              <List>
-                <h3 style={{textAlign:"center"}}>Priority</h3>
-                {priority.map(article => {
-                  return (         
-                  <ListItem key={article._id}>
-                      <a className="article-url" href={article.url}>
-                      <strong><h4> {article.title} </h4></strong></a>
-                       <p> Viewed: {article.date.split("T")[0]} </p>  
-                      
-                          <p>Tags:</p>
-                          <ul>{article.tags.length === 0 ? 
-                             <form style={{padding:"12px"}} onSubmit={(e) => {
-                              this.handleSubmit(e, article._id);
-                             }}>
-                             <input type="text" onChange={this.handleTagChange} className="form-control" placeholder="Enter a tag"/>
-                             </form>
-                            :
-                            article.tags.map((tag, i)=> <li key={i}>{tag}</li>)
-                            }
-                          </ul>
-                          <IconsContainer noteId="note" note={article.note}/>
-                          <IconsContainer value={article.favorited} favoriteId="favorite"> 
-                            <FavoriteBtn type="favorite" value={article.favorited} onClick={() => this.favoriteArticle(article._id, !article.favorited)}/> 
-                          </IconsContainer>
-                          <IconsContainer backlogId="backlog">
-                          <BacklogBtn type="toBacklog" onClick={() => this.saveForLater(article._id, true)} />
-                          </IconsContainer>
-                          <DeleteBtn onClick={() => this.deleteArticle(article._id)} />
-                </ListItem>
-                  );
-                })}
-              </List>
-              )
-              : (
-                <h3 style={{textAlign:"center"}}>Priority</h3>
-              )}
+              {priority.length ? this.renderLeftArticles(priority) : 
+                // if there are no articles in the priority array, display a standalone h3 tag
+                (<h3 style={{textAlign:"center"}}>Priority</h3>)}
               </Col>
               <Col styleProp="mid-articles" size="md-4">
               {backlog.length ? (
@@ -188,7 +192,7 @@ class Articles extends React.Component {
                 </List>
               ) : (
                   <h3 style={{textAlign:"center"}}>Nothing on backlog yet</h3>
-                )} */}
+                )}
                 </Col>
                 <Col styleProp="right-articles" size="md-4">
                 <SearchBar style={{width:"100%", clear:"both"}} value={this.state.search}
