78fd2078e85703fdd946f1f522c626751af030ff
Dariell Vasquez <dariellv@yahoo.com>
2018-11-13 01:02:21 -0500

    Unit test for Fetch_Articles reducer working

---
+++ client/src/reducers/__tests__/articles.test.js
@@ -0,0 +1,60 @@
+import articlesReducer from "../reducer_articles";
+import {
+    FETCH_ARTICLES,
+    BACKLOG_ARTICLE,
+    FAVORITE_ARTICLE,
+    DELETE_ARTICLE
+    } from "../../actions";
+import _ from "lodash";
+
+const articles = [
+        {
+            date: "2018-11-04T18:38:50.758Z",
+            email: "dariellv7@gmail.com",
+            favorited: false,
+            note: "Read before applying for positions",
+            saveForLater: false,
+            tags: ["Tech", "Javascript"],
+            title: "ES6",
+            url: "https://github.com/DrkSephy/es6-cheatsheet",
+            _id: "5bdf3cbac9c86c12773555be"
+        },
+       {
+            date: "2018-11-04T18:38:50.758Z",
+            email: "dariellv7@gmail.com",
+            favorited: false,
+            note: "Read before applying for positions",
+            saveForLater: false,
+            tags: ["Tech", "Javascript"],
+            title: "JS sorting algorithms",
+            url: "http://khan4019.github.io/front-end-Interview-Questions/sort.html#quickSort",
+            _id: "5bdf3cbac9c86c12773555bf"
+        },
+        {
+            date: "2018-11-04T18:38:50.758Z",
+            email: "dariellv7@gmail.com",
+            favorited: false,
+            note: "Possible study material after graduation",
+            saveForLater: false,
+            tags: [],
+            title: "AI nanodegree term one",
+            url: "https://medium.com/udacity/ai-nanodegree-program-syllabus-term-1-in-depth-80c41297acaf",
+            _id: "5bdf3cbac9c86c12773555c0"
+        }
+    ]
+it("handles actions of type FETCH_ARTICLES", ()=>{
+    const action = {
+        type: FETCH_ARTICLES,
+        payload :{
+            data: articles
+        }
+    }
+    const newState = articlesReducer({}, action);
+    expect(newState).toEqual(_.mapKeys(articles, "_id"));
+})
+
+
+it("handles action with unknown type", ()=>{
+    const newState = articlesReducer({}, {});
+    expect(newState).toEqual({})
+})
