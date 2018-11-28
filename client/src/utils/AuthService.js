5c324e82f6c508f487a9ba3f9d38ee91c499596f
Dariell Vasquez <dariellv@yahoo.com>
2018-11-28 16:03:25 -0500

    vulnerabilities fix

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -5,8 +5,8 @@ import CLIENT_ID from "./key";
 const ID_TOKEN_KEY = 'id_token';
 const ACCESS_TOKEN_KEY = 'access_token';
 const CLIENT_DOMAIN = 'rembr-app.auth0.com';
-const REDIRECT = 'https://rembr-app.herokuapp.com/callback';
-// const REDIRECT = 'http://localhost:3000/callback';
+// const REDIRECT = 'https://rembr-app.herokuapp.com/callback';
+const REDIRECT = 'http://localhost:3000/callback';
 const SCOPE = 'openid profile email';
 const AUDIENCE = 'https://rembr-app.auth0.com/api/v2/';
 
