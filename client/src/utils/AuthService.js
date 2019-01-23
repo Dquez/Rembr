b515d0a9aec678abe35a17990f1b25c4c942653d
Dariell Vasquez <dariellv@yahoo.com>
2019-01-23 14:48:45 -0500

    Migrated server/models files to separate server folder, altered start script b/c of migration, added docker files for nginx, server

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -6,8 +6,8 @@ const ID_TOKEN_KEY = 'id_token';
 const ACCESS_TOKEN_KEY = 'access_token';
 const CLIENT_DOMAIN = 'rembr-app.auth0.com';
 const CLIENT_ID = 'F3bv6-7PmyBo-GCCxiyewVN_Yf-CPOlg';
-const REDIRECT = 'https://rembr-app.herokuapp.com/callback';
-// const REDIRECT = 'http://localhost:3000/callback';
+// const REDIRECT = 'https://rembr-app.herokuapp.com/callback';
+const REDIRECT = 'http://localhost:3000/callback';
 const SCOPE = 'openid profile email';
 const AUDIENCE = 'https://rembr-app.auth0.com/api/v2/';
 
