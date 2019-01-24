a9b71b623fe35a2cbbe9121c66a9ae3a6c157fc8
Dariell Vasquez <dariellv@yahoo.com>
2019-01-24 13:25:40 -0500

    changed port from 8080 to 3000 for nginx, took out api url trim in conf file

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
   
 const articlesController = require("./controllers/articlesController");
 // Serve up static assets
-app.use(express.static("client/build"));
+app.use(express.static("../client/build"));
 // Add routes, both API and view
 app.use(articlesController);
 
